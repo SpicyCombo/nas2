65597021aad8dbe5739b0d63c86c50e4b71fde26
SpicyCombo <63688135+SpicyCombo@users.noreply.github.com>
2021-07-28 18:44:29 -0700

    hopefully tree fixed

--- nas2/NasTreeGens.cs
+++ nas2/NasTreeGens.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using MCGalaxy;
+using BlockID = System.UInt16;
+using MCGalaxy.Blocks;
 using MCGalaxy.Generator.Foliage;
 
 namespace NotAwesomeSurvival
@@ -21,7 +23,7 @@ namespace NotAwesomeSurvival
         public override void Generate(ushort x, ushort y, ushort z, TreeOutput output)
         {
             for (ushort dy = 0; dy < height + size - 1; dy++)
-                output(x, (ushort)(y + dy), z, /*LOG ID HERE*/ 250);
+                output(x, (ushort)(y + dy), z, /*LOG ID HERE*/ (byte)Block.FromRaw(250));
 
             for (int dy = -size; dy <= size; ++dy)
                 for (int dz = -size; dz <= size; ++dz)
@@ -33,93 +35,9 @@ namespace NotAwesomeSurvival
                             ushort xx = (ushort)(x + dx), yy = (ushort)(y + dy + height), zz = (ushort)(z + dz);
 
                             if (xx != x || zz != z || dy >= size - 1)
-                                output(xx, yy, zz, /*LEAVES ID HERE*/ 146);
+                                output(xx, yy, zz, /*LEAVES ID HERE*/ (byte)Block.FromRaw(140));
                         }
                     }
-        }
-    }
-
-    public sealed class ClassicTree : Tree
-    {
-
-        public override long EstimateBlocksAffected() { return height + 65; }
-
-        public override int DefaultSize(Random rnd) { return rnd.Next(3, 7); }
-
-        public override void SetData(Random rnd, int value)
-        {
-            height = value;
-            size = 2;
-            this.rnd = rnd;
-        }
-
-        public override void Generate(ushort x, ushort y, ushort z, TreeOutput output)
-        {
-            for (int dy = 0; dy <= height; dy++)
-                output(x, (ushort)(y + dy), z, Block.Log);
-
-            for (int dy = height - 2; dy <= height + 1; dy++)
-            {
-                int extent = dy > height - 1 ? 1 : 2;
-                for (int dz = -extent; dz <= extent; dz++)
-                    for (int dx = -extent; dx <= extent; dx++)
-                    {
-                        ushort xx = (ushort)(x + dx), yy = (ushort)(y + dy), zz = (ushort)(z + dz);
-                        if (xx == x && zz == z && dy <= height) continue;
-
-                        if (Math.Abs(dx) == extent && Math.Abs(dz) == extent)
-                        {
-                            if (dy > height) continue;
-                            if (rnd.Next(2) == 0) output(xx, yy, zz, Block.Leaves);
-                        }
-                        else
-                        {
-                            output(xx, yy, zz, Block.Leaves);
-                        }
-                    }
-            }
-        }
-    }
-
-    public sealed class SwampTree : Tree
-    {
-
-        public override long EstimateBlocksAffected() { return height + 145; }
-
-        public override int DefaultSize(Random rnd) { return rnd.Next(4, 8); }
-
-        public override void SetData(Random rnd, int value)
-        {
-            height = value;
-            size = 3;
-            this.rnd = rnd;
-        }
-
-        public override void Generate(ushort x, ushort y, ushort z, TreeOutput output)
-        {
-            for (int dy = 0; dy <= height; dy++)
-                output(x, (ushort)(y + dy), z, Block.Log);
-
-            for (int dy = height - 2; dy <= height + 1; dy++)
-            {
-                int extent = dy > height - 1 ? 2 : 3;
-                for (int dz = -extent; dz <= extent; dz++)
-                    for (int dx = -extent; dx <= extent; dx++)
-                    {
-                        ushort xx = (ushort)(x + dx), yy = (ushort)(y + dy), zz = (ushort)(z + dz);
-                        if (xx == x && zz == z && dy <= height) continue;
-
-                        if (Math.Abs(dx) == extent && Math.Abs(dz) == extent)
-                        {
-                            if (dy > height) continue;
-                            if (rnd.Next(2) == 0) output(xx, yy, zz, Block.Leaves);
-                        }
-                        else
-                        {
-                            output(xx, yy, zz, Block.Leaves);
-                        }
-                    }
-            }
         }
     }
 }
